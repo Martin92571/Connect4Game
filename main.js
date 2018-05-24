47a5ee6be7f54afbdf07a10be68f64dcb08181e5
Martin92571 <zegrizo@gmail.com>
2018-05-23 17:29:25 -0700

    Added the logic check

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ var lastTokenLocationX=null;
 var lastTokenLocationY=null;
 var currentTokenLocation = [
     ['', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '']
+    ['', '', '', 'red', '', '', ''],
+    ['red', '', '', 'red', '', '', ''],
+    ['', '', 'red', 'red', 'red', '', 'red'],
+    ['red', '', '', 'red', '', '', ''],
+    ['red', '', '', '', '', '', ''],
+    ['red', '', '', '', '', '', '']
 ];
 
 function currentPlayerToken(){
@@ -27,3 +27,108 @@ function currentPlayerToken(){
   }
 
 };
+
+function checkConnectFour(){
+  var xCordinate=lastTokenLocationX;
+  var yCordinate=lastTokenLocationY;
+  var oppositePlayer=currentPlayer;
+  switch(countDirection){
+    case "horizontalRight":
+      counter++
+      xCordinate++
+    while(yCordinate>=0 && yCordinate<=6 && xCordinate>=0 && xCordinate<=6 && currentTokenLocation[yCordinate][xCordinate]===playerColor[currentPlayer]){
+       xCordinate++
+       counter++;
+      }
+    countDirection="horizontalLeft";
+    if(winner()){return};
+    checkConnectFour();
+    break;
+
+    case "horizontalLeft":
+      xCordinate--
+      while(yCordinate>=0 && yCordinate<=6 && xCordinate>=0 && xCordinate<=6 && currentTokenLocation[yCordinate][xCordinate]===playerColor[currentPlayer]){
+      xCordinate--
+       counter++;
+      }
+      countDirection="verticalUp";
+      if(winner()){return};
+      counter=0;
+      checkConnectFour();
+      break;
+
+      case "verticalUp":
+      counter++
+      yCordinate--
+      while(yCordinate>=0 && yCordinate<=6 && xCordinate>=0 && xCordinate<=6 && currentTokenLocation[yCordinate][xCordinate]===playerColor[currentPlayer]){
+      yCordinate--
+       counter++;
+      }
+      countDirection="verticalDown";
+      if(winner()){return};
+      checkConnectFour();
+      break;
+
+      case "verticalDown":
+      yCordinate++
+      while(yCordinate>=0 && yCordinate<=6 && xCordinate>=0 && xCordinate<=6 && currentTokenLocation[yCordinate][xCordinate]===playerColor[currentPlayer]){
+      yCordinate++
+       counter++;
+      }
+      countDirection="diagnolTopRight";
+      if(winner()){return};
+      counter=0;
+      checkConnectFour();
+      break;
+
+      case "diagnolTopRight":
+      counter++
+      yCordinate--; xCordinate++;
+      while(yCordinate>=0 && yCordinate<=6 && xCordinate>=0 && xCordinate<=6 && currentTokenLocation[yCordinate][xCordinate]===playerColor[currentPlayer]){
+      yCordinate--; xCordinate++;
+       counter++;
+      }
+      countDirection="diagnolbottomLeft";
+      if(winner()){return};
+      checkConnectFour();
+      break;
+
+      case "diagnolbottomLeft":
+      yCordinate++; xCordinate--;
+      while(yCordinate>=0 && yCordinate<=6 && xCordinate>=0 && xCordinate<=6 && currentTokenLocation[yCordinate][xCordinate]===playerColor[currentPlayer]){
+      yCordinate++; xCordinate--;
+       counter++;
+      }
+      countDirection="diagnolTopLeft";
+      if(winner()){return};
+      counter=0;
+      checkConnectFour();
+      break; 
+
+      case "diagnolTopLeft":
+      counter++
+      yCordinate--; xCordinate--;
+      while(yCordinate>=0 && yCordinate<=6 && xCordinate>=0 && xCordinate<=6 && currentTokenLocation[yCordinate][xCordinate]===playerColor[currentPlayer]){
+      yCordinate--; xCordinate--;
+       counter++;
+      }
+      countDirection="diagnobottomRight";
+      if(winner()){return};
+      counter=0;
+      checkConnectFour();
+      break;
+
+      case "diagnobottomRight":
+      yCordinate++; xCordinate++;
+      while(yCordinate>=0 && yCordinate<=6 && xCordinate>=0 && xCordinate<=6 && currentTokenLocation[yCordinate][xCordinate]===playerColor[currentPlayer]){
+      yCordinate++; xCordinate++;
+       counter++;
+      }
+      countDirection="horizontalRight";
+      if(winner()){return};
+      counter=0;
+      break;            
+    }
+   
+  
+}
